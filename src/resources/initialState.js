a091c124d0574aa7b367f99b49a39c6095e767ce
Alphasam123 <Alphasam123@gmail.com>
2021-05-13 02:40:11 +0100

    add game examples

---
+++ src/resources/initialState.js
@@ -0,0 +1,11 @@
+/* eslint-disable quotes */
+var lobby;
+
+var players = lobby.players;
+for (var i = 0; i < players.length; i++) {
+    players[i] = {
+        playerId: players[i]._id,
+        view: [{ type: 'Card', data: 'hello' }],
+    };
+}
+JSON.stringify({ playerViews: players });
--- src/resources/onAction.js
+++ src/resources/onAction.js
@@ -1,8 +1,13 @@
+/* eslint-disable quotes */
 var playerViews;
 var context;
 
 for (var i = 0; i < playerViews.length; i++) {
     if (playerViews[i].playerId === context.playerId) {
+        playerViews[i].view = {
+            type: 'Card',
+            data: JSON.stringify(context),
+        };
     }
 }
 
